eea9eadc519491b2efb6647fa97916d7cdefe449
ludovic <ludovic.bade@gmail.com>
2024-08-09 12:27:14 +0200

    comment scw

--- provider.tf
+++ provider.tf
@@ -1,8 +1,15 @@
 # Scaleway
-provider "scaleway" {
-  access_key      = var.scw_access_key
-  secret_key      = var.scw_secret_key
-  organization_id = var.scw_organization_id
-  project_id      = var.scw_project_id
-}
+#provider "scaleway" {
+#  access_key      = var.scw_access_key
+#  secret_key      = var.scw_secret_key
+#  organization_id = var.scw_organization_id
+#  project_id      = var.scw_project_id
+#}
 
+# Scaleway
+provider "scaleway" {
+  access_key      = "SCW4GJQV9XZ2DATFC5FW"
+  secret_key      = "852198bc-ef39-4cf5-857d-49aa4deb770d"
+  organization_id = "54887954-604d-49fe-a456-e8645edec997"
+  project_id      = "54887954-604d-49fe-a456-e8645edec997"
+}
--- variables.tf
+++ variables.tf
@@ -2,26 +2,25 @@
 # Scaleway
 #
 
-variable "scw_access_key" {
-  type        = string
-  description = "Scaleway access key."
-}
+#variable "scw_access_key" {
+#  type        = string
+#  description = "Scaleway access key."
+#}
 
-variable "scw_secret_key" {
-  type        = string
-  description = "Scaleway secret key."
-}
+#variable "scw_secret_key" {
+#  type        = string
+#  description = "Scaleway secret key."
+#}
 
-variable "scw_organization_id" {
-  type        = string
-  description = "Scaleway organization ID."
-}
+#variable "scw_organization_id" {
+#  type        = string
+#  description = "Scaleway organization ID."
+#}
 
-variable "scw_project_id" {
-  type        = string
-  description = "Scaleway organization ID."
-}
-
+#variable "scw_project_id" {
+#  type        = string
+#  description = "Scaleway organization ID."
+#}
 
 variable "zone" {
   type = string
